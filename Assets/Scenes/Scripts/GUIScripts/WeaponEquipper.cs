110da8a49643ee3b599418be20f0a78a0b5c38e7
Julian Tuminaro <jtuminar@uci.edu>
2016-08-29 17:36:39 -0700

    work in progress

---
+++ Assets/Scenes/Scripts/GUIScripts/WeaponEquipper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponEquipper : MonoBehaviour {
+	public int m_weapon_number;
+	public GameObject stamp;
+
+	public void OnClick() {
+		stamp.transform.position.Set ((m_weapon_number - 1) * 340, 200, 0);
+	}
+}
